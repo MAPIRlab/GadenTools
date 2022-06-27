89cf29d5abb1adc7ec31ab571f0fee5c205c7815
Pepe Ojeda <ojedamorala@hotmail.com>
2022-06-27 09:39:48 +0200

    renamed folders

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 from Cython.Build import cythonize
 
 setup(
-    name='gaden',
-    ext_modules = cythonize(["src/Simulation.pyx", "src/Utils.pyx"]),
+    name='gadentools',
+    ext_modules = cythonize(["gadentools/Simulation.pyx", "gadentools/Utils.pyx"]),
     version='0.1.0',
     description='Python API to directly access GADEN simulation results, without depending on ROS.',
     author='Pepe Ojeda',
