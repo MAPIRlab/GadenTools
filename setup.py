e705928f7da462508c1fd210c57a70f9f7800718
Pepe Ojeda <ojedamorala@hotmail.com>
2023-04-12 12:54:34 +0200

    added numpy includes explicitly

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import find_packages, setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
+import numpy
+
+extensions = Extension("*", ["gadentools/*.pyx"],
+    include_dirs=[numpy.get_include()]
+)
 
 setup(
     name='gadentools',
-    ext_modules = cythonize(["gadentools/Simulation.pyx", "gadentools/Utils.pyx"]),
+    ext_modules = cythonize([extensions]),
     version='0.1.0',
     description='Python API to directly access GADEN simulation results, without depending on ROS.',
     author='Pepe Ojeda',
